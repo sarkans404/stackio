45d811364529e3d094b60ffbfe664cdcc3df4114
sarkans404 <rosuradic00@gmail.com>
2026-03-08 19:01:50 +0200

    feat: add response table and migration

---
+++ database/migrations/2026_03_08_153754_create_responses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('question_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('parent_id')
+                ->constrained('responses')
+                ->cascadeOnDelete();
+
+            $table->text('body');
+            $table->boolean('is_accepted')->default('true');
+            $table->boolean('is_correct')->default('false');
+            $table->string('image')->nullable();
+            
+            $table->integer('upvotes')->default(0);
+            $table->integer('downvotes')->default(0);
+            $table->integer('reports')->default(0);
+
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('responses');
+    }
+};
