eba95378a76523d2a1436e1847f87f5c250717ba
sarkans404 <rosuradic00@gmail.com>
2026-03-09 22:45:00 +0200

    refactor: update QuestionController methods for improved functionality and clarity

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,15 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
-    public function index(){
+    public function index($id)
+    {
+        $question = Question::with([
+            'user',
+            'images',
+            'responses.user',
+            'responses.children.user',
+        ])->findOrFail($id);
+        $questionsQty = Question::count();
+
+        return view('question', compact('question', 'questionsQty'));
+    }
+
+    public function createShow()
+    {
         return view('question-create');
     }
 
-    public function create(){
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+        ]);
+        $question = Question::create([
+            'user_id' => Auth::id(),
+            'title' => $request->title,
+            'body' => $request->text,
+        ]);
 
+        return redirect()->route('question.show', $question->id);
     }
 }
