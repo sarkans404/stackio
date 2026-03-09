4bce76afb6a4d351127602e040087d016dc4fb57
sarkans404 <rosuradic00@gmail.com>
2026-03-09 22:48:35 +0200

    feat: update user profile link to include user ID as a parameter

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -44,7 +44,7 @@
                     d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
             </svg>
         </a>
-        <a href="{{ route('user.profile') }}"
+        <a href="{{ route('user.profile', 1) }}"
             class="w-10 h-10 flex items-center justify-center rounded-full hover:text-blue-500 dark:hover:text-yellow-500 hover:bg-gray-200 dark:hover:bg-[#313232] group duration-200">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-6">
