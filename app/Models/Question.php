3d260b532a9a2eec5c01f637a614aa0d4548b65e
sarkans404 <rosuradic00@gmail.com>
2026-03-09 22:45:20 +0200

    feat: enhance Question model with relationships and fillable attributes

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'parent_id',
+        'title',
+        'body',
+        'tags',
+        'upvotes',
+        'downvotes',
+        'reports',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Responses::class)
+            ->whereNull('parent_id');
+    }
+
+    public function images()
+    {
+        return $this->hasMany(QuestionImages::class);
+    }
 }
