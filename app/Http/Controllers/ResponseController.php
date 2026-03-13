dc26997cde1ceac805063136c70ed62dc5ba6c66
sarkans404 <rosuradic00@gmail.com>
2026-03-13 08:21:51 +0200

    refactor: refactor ResponseController methods

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -21,9 +21,44 @@ class ResponseController extends Controller
             'parent_id' => $request->parent_id,
             'body' => $request->body,
         ]);
+
         $question = Question::findOrFail($request->question_id);
-        $question->answers += 1;
-        $question->save();
+        if ($request->parent_id === null) {
+            $question->answers += 1;
+            $question->save();
+        }
+
+        return redirect()->route('question.show', $request->question_id);
+    }
+
+    public function delete(Request $request)
+    {
+        $response = Responses::findOrFail($request->response_id);
+        if ($response->user_id === Auth::id() || Auth::user()->role === 'admin') {
+            Responses::where('id', $request->response_id)->delete();
+        }
+
+        $question = Question::findOrFail($request->question_id);
+        if ($response->parent_id === null) {
+            $question->answers -= 1;
+            $question->save();
+        }
+
+        return redirect()->route('question.show', $request->question_id);
+    }
+
+    public function edit(Request $request)
+    {
+        $request->validate([
+            'body' => 'required|min:3',
+        ]);
+
+        $response = Responses::findOrFail($request->response_id);
+
+        if ($response->user_id === Auth::id()) {
+            $response->body = $request->body;
+            $response->save();
+        }
 
         return redirect()->route('question.show', $request->question_id);
     }
