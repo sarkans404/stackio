c735fa0b1ef49e67ee8a9b55b2be6a05cbf68b02
sarkans404 <rosuradic00@gmail.com>
2026-03-13 08:29:26 +0200

    fix: reset initial values in QuestionsSeeder

--- database/seeders/QuestionsSeeder.php
+++ database/seeders/QuestionsSeeder.php
@@ -13,8 +13,8 @@ class QuestionsSeeder extends Seeder
     public function run(): void
     {
         DB::table('questions')->insert([
-            ['user_id' => 1, 'title' => 'How to implement a binary search algorithm in Python?', 'body' => 'I am trying to implement a binary search algorithm in Python, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?', 'views' => 20, 'upvotes' => 15, 'downvotes' => 2, 'answers' => 11],
-            ['user_id' => 1, 'title' => 'How to implement a binary search algorithm in C#?', 'body' => 'I am trying to implement a binary search algorithm in C#, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?', 'views' => 243, 'upvotes' => 165, 'downvotes' => 19, 'answers' => 1],
+            ['user_id' => 1, 'title' => 'How to implement a binary search algorithm in Python?', 'body' => 'I am trying to implement a binary search algorithm in Python, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?', 'views' => 0, 'upvotes' => 0, 'downvotes' => 0, 'answers' => 1],
+            ['user_id' => 1, 'title' => 'How to implement a binary search algorithm in C#?', 'body' => 'I am trying to implement a binary search algorithm in C#, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?', 'views' => 0, 'upvotes' => 0, 'downvotes' => 0, 'answers' => 4],
         ]);
     }
 }
