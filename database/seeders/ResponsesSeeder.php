72c01a12d0b575aa6d394d195b6902f8bbfc0ab7
sarkans404 <rosuradic00@gmail.com>
2026-03-13 08:29:57 +0200

    fix: reset initial value in ResponsesSeeder

--- database/seeders/ResponsesSeeder.php
+++ database/seeders/ResponsesSeeder.php
@@ -13,9 +13,9 @@ class ResponsesSeeder extends Seeder
     public function run(): void
     {
         FacadesDB::table('responses')->insert([
-            ['user_id' => 1, 'question_id' => 2, 'parent_id' => null, 'body' => 'Response TEST 1', 'upvotes' => 3, 'downvotes' => 1, 'image' => 'https://placeholdit.com/300x300/dddddd/999999?text=USER+IMAGE'],
-            ['user_id' => 1, 'question_id' => 2, 'parent_id' => null, 'body' => 'Response TEST 11', 'upvotes' => 3, 'downvotes' => 1, 'image' => null],
-            ['user_id' => 1, 'question_id' => 1, 'parent_id' => null, 'body' => 'Response TEST 0', 'upvotes' => 3, 'downvotes' => 1, 'image' => null],
+            ['user_id' => 1, 'question_id' => 2, 'parent_id' => null, 'body' => 'Response TEST 1', 'upvotes' => 3, 'downvotes' => 0, 'image' => 'https://placeholdit.com/300x300/dddddd/999999?text=USER+IMAGE'],
+            ['user_id' => 1, 'question_id' => 2, 'parent_id' => null, 'body' => 'Response TEST 11', 'upvotes' => 1, 'downvotes' => 4, 'image' => null],
+            ['user_id' => 1, 'question_id' => 1, 'parent_id' => null, 'body' => 'Response TEST 0', 'upvotes' => 2, 'downvotes' => 2, 'image' => null],
             ['user_id' => 1, 'question_id' => 2, 'parent_id' => 1, 'body' => 'Response TEST 2 Comment', 'upvotes' => 0, 'downvotes' => 2, 'image' => null],
             ['user_id' => 1, 'question_id' => 2, 'parent_id' => 1, 'body' => 'Response TEST 22 Comment', 'upvotes' => 0, 'downvotes' => 2, 'image' => null],
         ]);
