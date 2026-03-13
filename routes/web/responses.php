490bc6cd2143b30406c12be1f7b8c8c07835e418
sarkans404 <rosuradic00@gmail.com>
2026-03-13 08:46:13 +0200

    feat: add routes for editing response

--- routes/web/responses.php
+++ routes/web/responses.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\ResponseController;
 
 Route::controller(ResponseController::class)->group(function () {
     Route::post('/response/create', 'create')->name('responses.create')->middleware('auth');
+    Route::post('/response/edit', 'edit')->name('responses.edit')->middleware('auth');
+    Route::post('/response/delete', 'delete')->name('responses.delete')->middleware('auth');
 });
