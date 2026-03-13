b545683b2ee5ca744892bf4eace429c49ae948f0
sarkans404 <rosuradic00@gmail.com>
2026-03-13 09:03:27 +0200

    feat: add search controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    public function search(Request $request)
+    {
+        $request->validate([
+            'search' => 'required|min:2',
+        ]);
+
+        $questions = Question::with('user', 'images')
+            ->where('title', 'like', '%'.$request->search.'%')
+            ->paginate(20);
+
+        $questionsQty = Question::count();
+
+        return view('search', compact('questions', 'questionsQty'));
+    }
 }
