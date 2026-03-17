e5f514ac055151bdabe7d46772e109eeb66559a4
sarkans404 <rosuradic00@gmail.com>
2026-03-17 21:17:51 +0200

    chore: set timestamps in question seeder

--- database/seeders/QuestionsSeeder.php
+++ database/seeders/QuestionsSeeder.php
@@ -13,8 +13,30 @@ class QuestionsSeeder extends Seeder
     public function run(): void
     {
         DB::table('questions')->insert([
-            ['user_id' => 1, 'title' => 'How to implement a binary search algorithm in Python?', 'body' => 'I am trying to implement a binary search algorithm in Python, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?', 'views' => 0, 'upvotes' => 0, 'downvotes' => 0, 'answers' => 1],
-            ['user_id' => 1, 'title' => 'How to implement a binary search algorithm in C#?', 'body' => 'I am trying to implement a binary search algorithm in C#, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?', 'views' => 0, 'upvotes' => 0, 'downvotes' => 0, 'answers' => 4],
+            [
+                'user_id' => 1,
+                'title' => 'How to implement a binary search algorithm in Python?',
+                'body' => 'I am trying to implement a binary search algorithm in Python, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?',
+                'views' => 0,
+                'upvotes' => 0,
+                'downvotes' => 0,
+                'answers' => 1,
+                'created_at' => now()->copy()->subYears(50),
+                'updated_at' => now()->copy()->subYears(50),
+            ],
+
+            [
+                'user_id' => 1,
+                'title' => 'How to implement a binary search algorithm in C#?',
+                'body' => 'I am trying to implement a binary search algorithm in C#, but I am having trouble understanding how to handle edge cases. Can someone provide an example implementation?',
+                'views' => 0,
+                'upvotes' => 0,
+                'downvotes' => 0,
+                'answers' => 4,
+                'created_at' => now()->copy()->subDays(3),
+                'updated_at' => now()->copy()->subDays(3),
+            ],
+
         ]);
     }
 }
