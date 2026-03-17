0b5a6c8d8449433be6c0913590624b2a620cd723
sarkans404 <rosuradic00@gmail.com>
2026-03-17 21:15:45 +0200

    feat: add action routes for questions

--- routes/web/questions.php
+++ routes/web/questions.php
@@ -5,8 +5,14 @@ use App\Http\Controllers\QuestionController;
 Route::controller(QuestionController::class)->group(function () {
     Route::get('/question/create', 'createShow')->name('question.create.show')->middleware('auth');
     Route::post('/question/create', 'create')->name('question.create')->middleware('auth');
-    Route::get('/questions/{id}', 'index')->name('question.show');
     Route::delete('/question/delete', 'delete')->name('question.delete')->middleware('auth');
-    Route::get('/question/delete', 'edit')->name('question.edit')->middleware('auth');
-    Route::post('/question/delete', 'edit')->name('question.edit.show')->middleware('auth');
+    Route::get('/question/edit/{id}', 'editShow')->name('question.edit.show')->middleware('auth');
+    Route::post('/question/edit', 'edit')->name('question.edit')->middleware('auth');
+    Route::post('/question/follow', 'follow')->name('question.follow')->middleware('auth');
+    Route::post('/question/save', 'save')->name('question.save')->middleware('auth');
+    Route::post('/question/hide', 'hide')->name('question.hide')->middleware('auth');
+    Route::post('/question/report', 'report')->name('question.report')->middleware('auth');
+    Route::post('/question/showless', 'showLess')->name('question.showless')->middleware('auth');
+
+    Route::get('/questions/{id}', 'index')->name('question.show');
 });
