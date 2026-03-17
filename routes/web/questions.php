887a6db24b8b2ede7c1680026e8416525627ff7f
sarkans404 <rosuradic00@gmail.com>
2026-03-17 17:30:32 +0200

    feat: add delete and edit routes for questions

--- routes/web/questions.php
+++ routes/web/questions.php
@@ -6,4 +6,7 @@ Route::controller(QuestionController::class)->group(function () {
     Route::get('/question/create', 'createShow')->name('question.create.show')->middleware('auth');
     Route::post('/question/create', 'create')->name('question.create')->middleware('auth');
     Route::get('/questions/{id}', 'index')->name('question.show');
+    Route::delete('/question/delete', 'delete')->name('question.delete')->middleware('auth');
+    Route::get('/question/delete', 'edit')->name('question.edit')->middleware('auth');
+    Route::post('/question/delete', 'edit')->name('question.edit.show')->middleware('auth');
 });
