7787c888001372124513a531e5cde670b080571c
sarkans404 <rosuradic00@gmail.com>
2026-03-17 21:13:59 +0200

    feat: add data var

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,14 @@
         class="main-content grid grid-cols-1 gap-10 duration-300 translate-x-20 sm:grid-cols-[2fr_1fr] max-w-7xl h-max mx-auto">
         <div class="">
             @forelse ($questions as $question)
-                <x-post-card :question="$question" />
+                <x-post-card :question="$question" :userVotes="$userVotes" />
             @empty
                 <p class="text-center my-10">No questions found.</p>
             @endforelse
         </div>
 
-        <x-aside-card :question="$question" :questionsQty="$questionsQty" />
+        <x-aside-card :question="$question" :questionsQty="$questionsQty" :popularTags="$popularTags" :answersQty="$answersQty" :commentQty="$commentQty"
+            :userQty="$userQty" />
 
     </section>
 @endsection
