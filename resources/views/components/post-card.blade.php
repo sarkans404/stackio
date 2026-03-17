e7453a11d4631757e1dfdce05746fce26a9549f0
sarkans404 <rosuradic00@gmail.com>
2026-03-17 17:29:11 +0200

    refactor: refactor components for vote func

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -8,7 +8,7 @@
             </div>
             <span class="font-medium text-neutral-700 dark:text-gray-300">{{ $question->user->username }}</span>
             &bull;
-            <span class="text-neutral-500 dark:text-gray-400">{{ $question->date }}</span>
+            <span class="text-neutral-500 dark:text-gray-400">{{ $question->updated_at->diffForHumans() }}</span>
         </div>
 
         <div class="relative">
@@ -69,14 +69,43 @@
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
                     </svg>
-
                     Report
                 </button>
+                @if (Auth::id() === $question->user->id)
+                    <form action="{{ route('question.edit.show') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="question_id" value="{{ $question->id }}" class="hidden">
+                        <button type="submit"
+                            class="w-full flex items-center gap-4 px-4 py-2 cursor-pointer bg-[#f8f8f8] dark:bg-[#101314] hover:bg-gray-200 dark:hover:bg-[#313232] duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                            </svg>
+                            Edit
+                        </button>
+                    </form>
+                @endif
+                @if (Auth::id() === $question->user->id || auth()->user()?->role === 'admin')
+                    <form action="{{ route('question.delete') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="question_id" value="{{ $question->id }}" class="hidden">
+                        <button type="submit"
+                            class="w-full flex items-center gap-4 px-4 py-2 cursor-pointer bg-[#f8f8f8] dark:bg-[#101314] hover:bg-gray-200 dark:hover:bg-[#313232] duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                            Delete
+                        </button>
+                    </form>
+                @endif
             </div>
         </div>
     </div>
 
-    <div class="">
+    <div class="mb-5">
         <h2 class="text-2xl font-semibold mb-4">{{ Str::limit($question->title, 100) }}</h2>
         <p class="text-gray-600 dark:text-gray-400">{{ Str::limit($question->body, 200) }}</p>
 
@@ -89,9 +118,9 @@
     </div>
 
     <div class="flex items-center gap-4">
-        <div class="flex items-center gap-2 dark:bg-[#333333] rounded-full bg-gray-300">
-            <button id="upvote"
-                class="dark:bg-[#333333] bg-gray-300 w-10 h-10 rounded-full dark:hover:bg-neutral-700 hover:bg-gray-400 flex items-center justify-center duration-300 cursor-pointer">
+        <div class="voteBox flex items-center gap-2 dark:bg-[#333333] rounded-full bg-gray-300">
+            <button id="upvote" data-question-id="{{ $question->id }}" data-type="up"
+                class="vote {{ ($userVotes[$question->id] ?? null) === 'up' ? 'text-blue-500 dark:text-yellow-500' : '' }} dark:bg-[#333333] bg-gray-300 w-10 h-10 rounded-full dark:hover:bg-neutral-700 hover:bg-gray-400 flex items-center justify-center duration-300 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                     viewBox="0 0 256 256">
                     <path
@@ -100,10 +129,10 @@
                 </svg>
             </button>
 
-            <span class="font-medium select-none">{{ $question->upvotes }}</span>
+            <span class="upvoteText font-medium select-none">{{ $question->upvotes }}</span>
 
-            <button id="downvote"
-                class="dark:bg-[#333333] bg-gray-300 w-10 h-10 rounded-full dark:hover:bg-neutral-700 hover:bg-gray-400 flex items-center justify-center duration-300 cursor-pointer">
+            <button id="downvote" data-question-id="{{ $question->id }}" data-type="down"
+                class="vote {{ ($userVotes[$question->id] ?? null) === 'down' ? 'text-blue-500 dark:text-yellow-500' : 'GAY' }} dark:bg-[#333333] bg-gray-300 w-10 h-10 rounded-full dark:hover:bg-neutral-700 hover:bg-gray-400 flex items-center justify-center duration-300 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                     viewBox="0 0 256 256">
                     <path
@@ -111,6 +140,8 @@
                     </path>
                 </svg>
             </button>
+
+            <span class="downvoteText hidden font-medium select-none">{{ $question->downvotes }}</span>
         </div>
 
         <a href="{{ route('question.show', $question->id) }}"
