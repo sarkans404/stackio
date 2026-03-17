57c43a85b2e6392525b81e85d6b5cce17445369c
sarkans404 <rosuradic00@gmail.com>
2026-03-17 21:19:09 +0200

    feat: add data var

--- resources/views/components/aside-card.blade.php
+++ resources/views/components/aside-card.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <x-activity-card :questionsQty="$questionsQty" />
+    <x-activity-card :questionsQty="$questionsQty" :answersQty="$answersQty" :commentQty="$commentQty" :userQty="$userQty" />
 
-    <x-popular-tags-card />
+    <x-popular-tags-card :popularTags="$popularTags" />
 
     <x-recent-card :question="$question" />
 </div>
