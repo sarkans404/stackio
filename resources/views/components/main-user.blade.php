abf74be004d142c27318554641c8e22087bf5b62
sarkans404 <rosuradic00@gmail.com>
2026-03-19 20:58:14 +0200

    chore: update routes into profile page

--- resources/views/components/main-user.blade.php
+++ resources/views/components/main-user.blade.php
@@ -22,26 +22,30 @@
                     </svg>
                 </span>
             @endif
-            <a href="{{ route('user.edit') }}"
-                class="text-lg dark:bg-yellow-600 bg-blue-500 py-1 px-5 rounded-xl font-semibold text-white">Edit</a>
+            @if ($user->id === Auth::id())
+                <a href="{{ route('user.edit') }}"
+                    class="text-lg dark:bg-yellow-600 bg-blue-500 py-1 px-5 rounded-xl font-semibold text-white">Edit</a>
+            @endif
         </div>
     </div>
     <div
         class="flex items-center gap-4 dark:text-white my-10 text-neutral-700 border-b pb-4 dark:border-neutral-600 border-neutral-400">
-        <a href="{{ route('user.profile', 1) }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Profile</a>
-        <a href="{{ route('user.questions') }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user/posts' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Questions</a>
-        <a href="{{ route('user.answers') }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user/comments' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Answers</a>
-        <a href="{{ route('user.saved') }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user/saved' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Saved</a>
-        <a href="{{ route('user.hidden') }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user/hidden' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Hidden</a>
-        <a href="{{ route('user.upvoted') }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user/upvoted' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Upvoted</a>
-        <a href="{{ route('user.downvoted') }}"
-            class="text-lg py-2 px-5 {{ request()->path() === 'user/downvoted' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Downvoted</a>
+        @if ($user->id === Auth::id())
+            <a href="{{ route('user.profile', $user->id) }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/' . $user->id ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Profile</a>
+            <a href="{{ route('user.questions') }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/questions' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Questions</a>
+            <a href="{{ route('user.answers') }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/answers' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Answers</a>
+            <a href="{{ route('user.saved') }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/saved' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Saved</a>
+            <a href="{{ route('user.hidden') }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/hidden' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Hidden</a>
+            <a href="{{ route('user.upvoted') }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/upvoted' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Upvoted</a>
+            <a href="{{ route('user.downvoted') }}"
+                class="text-lg py-2 px-5 {{ request()->path() === 'user/downvoted' ? 'dark:bg-neutral-700 bg-gray-300' : '' }} cursor-pointer dark:hover:bg-neutral-600 hover:bg-gray-400 duration-300 rounded-full font-semibold">Downvoted</a>
+        @endif
     </div>
     <div>
         {{ $slot }}
