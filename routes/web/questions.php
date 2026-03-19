39da15d0d21077808a8960b8d69f06e66b604ff9
sarkans404 <rosuradic00@gmail.com>
2026-03-19 21:02:30 +0200

    chore: remove useless routes

--- routes/web/questions.php
+++ routes/web/questions.php
@@ -8,10 +8,6 @@ Route::controller(QuestionController::class)->group(function () {
     Route::delete('/question/delete', 'delete')->name('question.delete')->middleware('auth');
     Route::get('/question/edit/{id}', 'editShow')->name('question.edit.show')->middleware('auth');
     Route::post('/question/edit', 'edit')->name('question.edit')->middleware('auth');
-    Route::post('/question/follow', 'follow')->name('question.follow')->middleware('auth');
-    Route::post('/question/hide', 'hide')->name('question.hide')->middleware('auth');
-    Route::post('/question/report', 'report')->name('question.report')->middleware('auth');
-    Route::post('/question/showless', 'showLess')->name('question.showless')->middleware('auth');
 
     Route::get('/questions/{id}', 'index')->name('question.show');
 });
