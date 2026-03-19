8ab64c68aad63cd5d0b98e75c5858f47d26741cd
sarkans404 <rosuradic00@gmail.com>
2026-03-19 17:42:33 +0200

    feat: add question save functionality

---
+++ resources/js/ajax/save.js
@@ -0,0 +1,48 @@
+import { initNotice } from '../components/actionNotice.js';
+
+export default function initSave(){
+     document.querySelectorAll('.saveBtn').forEach((btn) => {       
+          btn.addEventListener('click', async () => {
+               const userId = btn.dataset.userId ?? null;
+               const questionId = btn.dataset.questionId ?? null;
+
+               try {
+                    const res = await fetch('/question/save', {
+                         url: '/question/save',
+                         method: 'POST',
+                         headers: {
+                              'Content-Type': 'application/json',
+                              'X-CSRF_TOKEN': document.querySelector('meta[name="csrf-token"]').content
+                         },
+                         body: JSON.stringify({
+                              user_id: userId,
+                              question_id: questionId,
+                         }),
+                    });
+
+                    const data = await res.json();
+                    const text = document.querySelector('.saveBtn>span');
+                    
+
+                    if(data.success){
+                         initNotice(data.success);
+
+                         if(data.action === 'unsaved'){
+                              btn.classList.remove('dark:text-yellow-500', 'text-blue-500')
+                              text.innerText = 'Save';
+                         }
+                         if(data.action === 'saved'){
+                              btn.classList.add('dark:text-yellow-500', 'text-blue-500');
+                              text.innerText = 'Saved';
+                         }
+                    }
+                    if(data.error){
+                         initNotice(data.error);
+                    }
+               } catch(err){
+                    initNotice('Catch an error on a save post!')
+                    console.log(err);
+               }
+          });
+     });
+}
