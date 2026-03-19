6a50159bf79408c9ce1e1b5e93a4c8bc6e241f3d
sarkans404 <rosuradic00@gmail.com>
2026-03-19 17:42:58 +0200

    refactor: add import initNotice into vote.js

--- resources/js/ajax/vote.js
+++ resources/js/ajax/vote.js
@@ -1,3 +1,5 @@
+import { initNotice } from '../components/actionNotice.js';
+
 export default function initVote(){
     
     document.querySelectorAll('.vote').forEach((btn) => {
@@ -61,18 +63,4 @@ export default function initVote(){
             }
         });
     });
-}
-
-function initNotice(message){
-     let notice = document.querySelector('#notice');
-     
-     let child = document.createElement('div');
-     child.classList.add('flex', 'items-center', 'py-2', 'px-4', 'rounded', 'dark:bg-neutral-600', 'bg-gray-300');
-     child.innerHTML += `<span class="text-medium">${message}</span>`;
-
-     notice.appendChild(child);
-
-     setTimeout(() => {
-          notice.removeChild(child);
-     }, 1300);
 }
