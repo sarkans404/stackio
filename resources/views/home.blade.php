ff0988d56b2de269f056c512e3370b1b149cf8fd
sarkans404 <rosuradic00@gmail.com>
2026-03-19 17:45:27 +0200

    chore: pass var to postCard component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         class="main-content grid grid-cols-1 gap-10 duration-300 translate-x-20 sm:grid-cols-[2fr_1fr] max-w-7xl h-max mx-auto">
         <div class="">
             @forelse ($questions as $question)
-                <x-post-card :question="$question" :userVotes="$userVotes" />
+                <x-post-card :question="$question" :userVotes="$userVotes" :userSaved="$userSaved" />
             @empty
                 <p class="text-center my-10">No questions found.</p>
             @endforelse
