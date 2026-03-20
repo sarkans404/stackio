f1ce856344778be08a939044270967c50e1a829d
sarkans404 <rosuradic00@gmail.com>
2026-03-20 18:06:10 +0200

    chore: import delete route

--- routes/web/users.php
+++ routes/web/users.php
@@ -3,15 +3,17 @@
 use App\Http\Controllers\UserController;
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/user/questions', 'userQuestions')->name('user.questions');
-    Route::get('/user/answers', 'userAnswers')->name('user.answers');
-    Route::get('/user/saved', 'userSaved')->name('user.saved');
-    Route::get('/user/hidden', 'userHidden')->name('user.hidden');
-    Route::get('/user/upvoted', 'userUpvoted')->name('user.upvoted');
-    Route::get('/user/downvoted', 'userDownvoted')->name('user.downvoted');
-    Route::get('/user/edit', 'userEdit')->name('user.edit');
-    Route::post('/user/edit', 'userUpdate')->name('user.update');
+    Route::get('/user/questions', 'userQuestions')->name('user.questions')->middleware('auth');
+    Route::get('/user/answers', 'userAnswers')->name('user.answers')->middleware('auth');
+    Route::get('/user/saved', 'userSaved')->name('user.saved')->middleware('auth');
+    Route::get('/user/hidden', 'userHidden')->name('user.hidden')->middleware('auth');
 
-    Route::get('/user/{id}', 'index')->name('user.profile')->middleware('auth');
+    Route::get('/user/upvoted', 'userUpvoted')->name('user.upvoted')->middleware('auth');
+    Route::get('/user/downvoted', 'userDownvoted')->name('user.downvoted')->middleware('auth');
+
+    Route::get('/user/edit', 'userEdit')->name('user.edit')->middleware('auth');
+    Route::post('/user/edit', 'update')->name('user.update')->middleware('auth');
+
+    Route::get('/user/{id}', 'index')->name('user.profile');
 }
 );
