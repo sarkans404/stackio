434a063a5b2c56f9e55696c778afe4f8656328f5
sarkans404 <rosuradic00@gmail.com>
2026-03-20 23:52:08 +0200

    chore: import gallery animation and upload functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,21 +3,25 @@ import "./bootstrap";
 import initTheme from "./modules/theme";
 import initSidebar from "./modules/sidebar";
 import initNavigation from "./modules/navigation";
+import initGallery from "./modules/gallery";
 import initMenus from "./modules/menus";
 import initComments from "./modules/comments";
 import initClipboard from "./modules/clipboard";
 import initBackButton from "./modules/backButton";
 import initEditResponses from "./modules/editResponses";
 import initAvatarPreview from "./modules/avatarPreview";
+import initImageUploadPreview from "./modules/uploadPreview";
 import initVote from "./ajax/vote";
 import initSave from "./ajax/save";
 import initHide from "./ajax/hide";
 import initClearRecent from "./ajax/clearRecent";
+import initUploadRemove from "./ajax/uploadRemove";
 
 document.addEventListener("DOMContentLoaded", () => {
     initTheme();
     initSidebar();
     initNavigation();
+    initGallery();
     initBackButton();
     initMenus();
     initComments();
@@ -28,4 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
     initSave();
     initHide();
     initClearRecent();
+    initImageUploadPreview();
+    initUploadRemove();
 });
