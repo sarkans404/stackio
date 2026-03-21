602de9653a8ef71e8ecbc428f6f212cd2e3debef
sarkans404 <rosuradic00@gmail.com>
2026-03-21 19:26:56 +0200

    chore: add fillable attributes for tags model

--- app/Models/Tags.php
+++ app/Models/Tags.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tags extends Model
 {
+    protected $fillable = [
+        'slug',
+        'name',
+    ];
+
     public function questions()
     {
         return $this->belongsToMany(Question::class, 'question_tags', 'tag_id', 'question_id');
