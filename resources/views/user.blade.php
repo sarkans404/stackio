d7f8ed4ab7c373224a1e15028b951ace33340d53
sarkans404 <rosuradic00@gmail.com>
2026-03-21 17:13:48 +0200

    chore: add reputation render

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,6 +8,9 @@
     <x-main-user :user="$user">
         <div>
             <div class="flex items-center gap-4 mb-5">
+                <span
+                    class="py-2 px-4 {{ $user->reputation > 100 ? 'dark:bg-yellow-600 bg-blue-500' : 'dark:bg-[#af9204] bg-cyan-500' }} font-medium dark:text-neutral-100 rounded-lg">
+                    {{ $user->reputation }} reputation</span>
                 <span class="py-2 px-4 dark:bg-neutral-600 bg-gray-300 dark:text-neutral-100 font-medium rounded-lg">
                     {{ $answerQty }} answers</span>
                 <span class="py-2 px-4 dark:bg-neutral-600 bg-gray-300 dark:text-neutral-100 font-medium rounded-lg">
@@ -29,7 +32,7 @@
                 @if ($user->bio)
                     <p class="dark:text-neutral-100 text-neutral-700">{{ $user->bio }}</p>
                 @else
-                    <p class="dark:text-neutral-100 text-neutral-700">You can add bio here...</p>
+                    <p class="dark:text-neutral-100 text-neutral-700">You can add bio...</p>
                 @endif
             </div>
         </div>
