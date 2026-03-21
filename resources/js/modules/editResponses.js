f1d3f32db87da30e839993ebc573daa615635b74
sarkans404 <rosuradic00@gmail.com>
2026-03-21 17:12:42 +0200

    chore: add show/hide functionality to response image container

--- resources/js/modules/editResponses.js
+++ resources/js/modules/editResponses.js
@@ -3,15 +3,18 @@ export default function initEditResponses(){
      const editForm = document.querySelectorAll('.edit-form');
      const editResetBtn = document.querySelectorAll('.edit-reset-btn');
      const responseBody = document.querySelectorAll('.response-body');
+     const responseImage = document.querySelectorAll('.responseImage');
 
      editBtn.forEach((btn, index) => {
           btn.addEventListener('click', () => {
                if(editForm[index].classList.contains('hidden')){
                     editForm[index]?.classList.remove('hidden');
                     responseBody[index]?.classList.add('hidden');
+                    responseImage[index]?.classList.add('hidden');
                } else {
                     editForm[index]?.classList.add('hidden');
                     responseBody[index]?.classList.remove('hidden');
+                    responseImage[index]?.classList.remove('hidden');
 
                }
           });
@@ -22,6 +25,7 @@ export default function initEditResponses(){
                if(!editForm[index].classList.contains('hidden')){
                     editForm[index]?.classList.add('hidden');
                     responseBody[index]?.classList.remove('hidden');
+                    responseImage[index]?.classList.remove('hidden');
                }
           });
      });
