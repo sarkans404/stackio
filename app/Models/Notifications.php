f5639d3e2842ddc15e5a30dd43df39b6baadaae7
sarkans404 <rosuradic00@gmail.com>
2026-03-23 01:16:37 +0200

    chore: add fillable attributes and relations to notifications model

--- app/Models/Notifications.php
+++ app/Models/Notifications.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notifications extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'actor_id',
+        'notifiable_id',
+        'notifiable_type',
+        'type',
+        'is_read',
+    ];
+
+    public function notifiable()
+    {
+        return $this->morphTo();
+    }
+
+    public function actor()
+    {
+        return $this->belongsTo(User::class, 'actor_id');
+    }
 }
