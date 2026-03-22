38ed7009254e4e7fcd50d13ff5df507bdf88f681
sarkans404 <rosuradic00@gmail.com>
2026-03-23 01:21:26 +0200

    chore: add notification route

--- routes/web/notifications.php
+++ routes/web/notifications.php
@@ -1,3 +1,8 @@
 <?php
 
-Route::get('/notification', [HomeController::class, 'notificationShow'])->name('notification.show');
+use App\Http\Controllers\NotificationController;
+
+Route::controller(NotificationController::class)->group(function () {
+    Route::get('/notification', 'index')->name('notification.show')->middleware('auth');
+    Route::get('/notification/read', 'markAllRead')->name('notification.read.all')->middleware('auth');
+});
